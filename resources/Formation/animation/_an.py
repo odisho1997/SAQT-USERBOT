2b1048dc7ac4754cdd921fd4b68b94657def82f4
odisho1997 <86355012+odisho1997@users.noreply.github.com>
2021-06-24 17:12:45 +0300

    Update _an.py

--- resources/Formation/animation/_an.py
+++ resources/Formation/animation/_an.py
@@ -7,7 +7,7 @@ from userbot.Config import Config
 
 USERID = Config.OWNER_ID 
 ALIVE_NAME = Config.ALIVE_NAME
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Icss Userbot"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "SAQT-USERBOT"
 mention = f"[{DEFAULTUSER}](tg://user?id={USERID})"
 
 #  ======================================================= #
